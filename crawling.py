1ad4bcf8f324c548bf505e7e171d3027b96a7dfa
ohyujeong <103317018+ujeongoh@users.noreply.github.com>
2023-05-11 05:41:12 +0900

    test7

--- crawling.py
+++ crawling.py
@@ -234,7 +234,7 @@ def programmers_crawl():
         while True:
             
             # 강의 섹션
-            section = driver.find_element(By.XPATH, '//*[@id="edu-service-app-main"]/div/div[2]/div/div/section[2]')
+            section = driver.find_element(By.CLASS_NAME, 'Learnstyle__CourseList-sc-f2nskl-11')
             for si in range(1, len(section.find_elements(By.TAG_NAME, "a")) + 1):
                 
                 # 모집 마감 제외

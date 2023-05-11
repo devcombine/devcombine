7534e7269c523a9e725400b7c168b6b1cd07a879
ohyujeong <103317018+ujeongoh@users.noreply.github.com>
2023-05-11 15:58:50 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
 from selenium.common.exceptions import NoSuchElementException
 from collections import defaultdict
 from selenium.webdriver import ActionChains

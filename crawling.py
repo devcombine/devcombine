32988f9c1332fe8271e09cf66c41c4a04ae80778
ohyujeong <103317018+ujeongoh@users.noreply.github.com>
2023-05-11 16:07:16 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -6,6 +6,7 @@ import csv
 import asyncio
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
